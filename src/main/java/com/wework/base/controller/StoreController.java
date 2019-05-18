595cb6486115ce7c7405cbc98128539b5b53663f
梁庆丰 <liangqf1@yonyou.com>
2019-05-18 10:33:40 +0800

    门店初始化

--- src/main/java/com/wework/base/controller/StoreController.java
+++ src/main/java/com/wework/base/controller/StoreController.java
@@ -1,20 +1,27 @@
 package com.wework.base.controller;
 
 import com.wework.base.domain.base.BaseJSON;
+import com.wework.base.domain.base.vo.StoreVO;
+import com.wework.base.service.StoreService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @RestController
 @EnableSwagger2
 @RequestMapping("/store")
 @Api(tags = { "门店模块" })
 public class StoreController {
+    @Autowired
+    StoreService storeService;
     @ApiOperation("查找门店")
     @ApiImplicitParams(value = {
             @ApiImplicitParam(paramType = "query", name = "longitude", dataType = "String", required = true, value = "经度", defaultValue = "116.40"),
@@ -24,6 +31,7 @@ public class StoreController {
     public BaseJSON findStoreList(String longitude, String latitude,String storeType) {
 
         BaseJSON baseJSON = new BaseJSON();
+        List<StoreVO> storeList = storeService.findStoreList();
         //baseJSON.setResult(account + "-" + password);
         return baseJSON;
     }
